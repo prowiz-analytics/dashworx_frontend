0c6159013686587442070e40c84db1158851bc25
Hemnath0075 <hemnath5700@gmail.com>
2024-05-27 22:00:04 +0530

    added name

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -123,7 +123,7 @@ function Home() {
             <div className="w-[95%] flex flex-row justify-start items-center border-t-[1px] ml-2">
               <p className="py-2 quicksand-font ">
                 <span className="font-[600] text-[#28262C]">Data Hub </span>|
-                {` Dashworx`} Limited
+                {`${process.env.REACT_APP_CLIENT}`} Limited
               </p>
             </div>
           </div>
